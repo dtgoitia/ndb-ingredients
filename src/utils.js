da26cd1f51c51a6a438944183ba6bb9f27c9c9d0
David Torralba Goitia <david.torralba.goitia@gmail.com>
2017-08-27 23:49:33 +0100

    axios implemented successfully

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,14 @@
+import axios from 'axios';
+
 const getNutritionData = (API) => {
-  console.log('API:', API);
-  return API
+  console.log('getNutritionData() executed');
+  axios.get(`https://www.reddit.com/r/reactjs.json`)
+  .then(res => {
+    console.log('res:', res);
+    // const posts = res.data.data.children.map(obj => obj.data);
+    // this.setState({ posts });
+  });    
+  return null
 }
 
 const utils = {
