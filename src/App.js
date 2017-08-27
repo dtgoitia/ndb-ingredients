cb194fbfd662c44cee5e0327d76984a461e78087
David Torralba Goitia <david.torralba.goitia@gmail.com>
2017-08-27 19:33:06 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+const React = require('react');
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi!</h1>
+        <p>I am an Electron app built in React!</p>
+      </div>
+    );
+  }
+}
+
+module.exports = App;
