7d716348469c100c3c3d66dee1f42192d9d2b1f0
David Torralba Goitia <david.torralba.goitia@gmail.com>
2017-08-31 18:29:02 +0100

    Day expansion and contraction properly managed

--- src/components/Week.js
+++ src/components/Week.js
@@ -4,19 +4,20 @@ import Day from './Day';
 class Week extends React.Component {
   constructor(props){
     super(props)
-    this.state = {}
+    this.state = {
+    }
 
     this.selectDay = this.selectDay.bind(this);
   }
   selectDay(day){
-    console.info(day + ' should expand!');
+    console.info(day + ' expanded!');
     document.documentElement.style.setProperty('--gridCurrent', 'var(--grid' + day + ')');
   }
 
   render() {
     return (
       <div className='week'>
-        <h2>{this.props.weekPlan.name}</h2>
+        <h2 onClick={()=>{this.selectDay('None')}}>{this.props.weekPlan.name}</h2>
         {this.props.weekPlan.weekPlan.map((dayPlan, i)=>{
           return (
             <Day
